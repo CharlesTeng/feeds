9e8bfeaa4285f91cc935e45c4d76efec793c2159
CharlesTeng <mingyi.deng@gmail.com>
2014-12-17 15:08:07 +0800

    first commit

---
+++ schd/src/schd.c
@@ -0,0 +1,58 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+#include <getopt.h>
+#include <libgen.h>
+
+#include "schd.h"
+//#include "watchdog.h"
+//#include "plug/hotplug.h"
+unsigned int debug;
+static int usage(const char *prog)
+{
+	ERROR("Usage: %s [options]\n"
+		"Options:\n"
+		"\t-s <path>\tPath to ubus socket\n"
+		"\t-h <path>\trun as hotplug daemon\n"
+		"\t-d <level>\tEnable debug messages\n"
+		"\n", prog);
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	int ch;
+	char *dbglvl = getenv("DBGLVL");
+
+	if (dbglvl) {
+		debug = atoi(dbglvl);
+		unsetenv("DBGLVL");
+	}
+
+	while ((ch = getopt(argc, argv, "d:s:h:")) != -1) {
+		switch (ch) {
+//		case 'h':
+//			return hotplug_run(optarg);
+		case 's':
+			ubus_socket = optarg;
+			break;
+		case 'd':
+			debug = atoi(optarg);
+			break;
+		default:
+			return usage(argv[0]);
+		}
+	}
+	uloop_init();
+//	schd_signal();
+//	trigger_init();
+	if (getpid() != 1)
+		schd_connect_ubus();
+//	else
+//		schd_state_next();
+	uloop_run();
+
+	return 0;
+}
---
+++ schd/src/ubus.c
@@ -0,0 +1,54 @@
+#include <sys/resource.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "schd.h"
+
+char *ubus_socket = NULL;
+static struct ubus_context *ctx;
+static struct uloop_timeout ubus_timer;
+
+
+static void
+ubus_reconnect_cb(struct uloop_timeout *timeout)
+{
+	if (!ubus_reconnect(ctx, ubus_socket))
+		ubus_add_uloop(ctx);
+	else
+		uloop_timeout_set(timeout, 2000);
+}
+
+static void
+ubus_disconnect_cb(struct ubus_context *ctx)
+{
+	ubus_timer.cb = ubus_reconnect_cb;
+	uloop_timeout_set(&ubus_timer, 2000);
+}
+
+static void
+ubus_connect_cb(struct uloop_timeout *timeout)
+{
+	ctx = ubus_connect(ubus_socket);
+
+	if (!ctx) {
+		DEBUG(4, "Connection to ubus failed\n");
+		uloop_timeout_set(&ubus_timer, 1000);
+		return;
+	}
+
+	ctx->connection_lost = ubus_disconnect_cb;
+//	ubus_init_service(ctx);
+//	ubus_init_system(ctx);
+//	watch_ubus(ctx);
+
+	DEBUG(2, "Connected to ubus, id=%08x\n", ctx->local_id);
+	ubus_add_uloop(ctx);
+}
+
+void
+schd_connect_ubus(void)
+{
+	ubus_timer.cb = ubus_connect_cb;
+	uloop_timeout_set(&ubus_timer, 1000);
+}
