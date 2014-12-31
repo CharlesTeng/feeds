d46786480bc43e1583fa9bb1f972c8672f07e65d
CharlesTeng <mingyi.deng@gmail.com>
2014-12-31 16:00:26 +0800

    implement ubus call schedule list

--- schd/src/service.c
+++ schd/src/service.c
@@ -18,39 +18,63 @@
 #include "schd.h"
 
 #include "service.h"
+#include <uci.h>
+
 static struct ubus_context *ctx;
+static struct blob_buf b;
 
-enum {
-	SERVICE_ATTR_NAME,
-	__SERVICE_ATTR_MAX,
-};
-
-static const struct blobmsg_policy service_attrs[__SERVICE_ATTR_MAX] = {
-	[SERVICE_ATTR_NAME] = { "name", BLOBMSG_TYPE_STRING },
-};
 
 static int
 service_handle_list(struct ubus_context *ctx, struct ubus_object *obj,
 		    struct ubus_request_data *req, const char *method,
 		    struct blob_attr *msg)
 {
-	struct blob_attr *tb[__SERVICE_ATTR_MAX];
-	int verbose = 0;
+	static struct uci_context *uctx;
+	struct uci_ptr ptr;
+	int ret = 0;
 
-	blobmsg_parse(service_attrs, __SERVICE_ATTR_MAX, tb, blob_data(msg), blob_len(msg));
-
-	if (tb[SERVICE_ATTR_NAME] && blobmsg_get_bool(tb[SERVICE_ATTR_NAME]))
-		verbose = 1;
-
-//	blob_buf_init(&b, 0);
-
-//	ubus_send_reply(ctx, req, b.head);
-
-	return 0;
+//  list the config/schedule 
+	blob_buf_init(&b, 0);
+	uctx = uci_alloc_context();
+	if (uci_lookup_ptr(uctx, &ptr, "schedule", true) != UCI_OK) {
+		ret = 1;
+	}
+	if(ret == 0){
+		//uci_export(uctx, stdout, ptr.p, true);
+		//-->uci_export_package(package, stream, header);
+		struct uci_package *p = ptr.p;
+		struct uci_element *s, *o, *i;
+		void *c;
+		
+		uci_foreach_element(&p->sections, s) {	
+			struct uci_section *sec = uci_to_section(s);
+			c = blobmsg_open_table(&b, sec->type);	
+			uci_foreach_element(&sec->options, o) {
+				struct uci_option *opt = uci_to_option(o);
+				switch(opt->type) {
+				case UCI_TYPE_STRING:
+					blobmsg_add_string(&b, opt->e.name, opt->v.string);
+					break;
+				case UCI_TYPE_LIST:
+					uci_foreach_element(&opt->v.list, i) {
+				// not support		
+					}
+					break;
+				default:
+				//	fprintf(stream, "\t# unknown type for option '%s'\n", uci_escape(ctx, opt->e.name));
+					break;
+				}
+			}
+			blobmsg_close_table(&b, c);
+		}
+	}
+	ubus_send_reply(ctx, req, b.head);
+	uci_free_context(uctx);
+	return ret;
 }
 
 static struct ubus_method main_object_methods[] = {
-	UBUS_METHOD("schedule_apply", service_handle_list, service_attrs),
+	UBUS_METHOD_NOARG("list", service_handle_list),
 };
 
 static struct ubus_object_type main_object_type =
